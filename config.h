59c65b4555c6ce72bcaecf97a18ac86da03c38a5
Ruslan Yupyn <r.yupyncrossplusplus.org>
2016-07-27 21:26:45 +0300

    # Object files

---
+++ config.h
@@ -0,0 +1,34 @@
+/*
+ * configuration for markdown, generated Wed Jul 27 21:26:16 EEST 2016
+ * by ruslan@Ruslans-MBP.local
+ */
+#ifndef __AC_MARKDOWN_D
+#define __AC_MARKDOWN_D 1
+
+
+#define OS_DARWIN 1
+#define THEME_CF 1
+#define while(x) while( (x) != 0 )
+#define if(x) if( (x) != 0 )
+#define DWORD unsigned int
+#define WORD unsigned short
+#define BYTE unsigned char
+#define HAVE_BASENAME 1
+#define HAVE_LIBGEN_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_ALLOCA_H 1
+#define HAVE_PWD_H 1
+#define HAVE_GETPWUID 1
+#define HAVE_SRANDOM 1
+#define INITRNG(x) srandom((unsigned int)x)
+#define HAVE_BZERO 1
+#define HAVE_RANDOM 1
+#define COINTOSS() (random()&1)
+#define HAVE_STRCASECMP 1
+#define HAVE_STRNCASECMP 1
+#define HAVE_FCHDIR 1
+#define TABSTOP 4
+#define PATH_FIND "/usr/bin/find"
+#define PATH_SED "/usr/bin/sed"
+
+#endif/* __AC_MARKDOWN_D */
---
+++ config.md
@@ -0,0 +1,5 @@
+#! /bin/sh
+# script generated Wed Jul 27 21:26:16 EEST 2016 by configure.sh
+
+test -d "$1" || mkdir -p "$1"
+exit 0
